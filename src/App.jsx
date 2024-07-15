49d246cf977ecd0397cf872cbf5ea9d524d98bc0
Shadowplayer118 <dhanielmalinao@gmail.com>
2024-07-15 20:29:38 +0800

    Designed Score board

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,10 @@ const App = () => {
         else{
             setCount1(team1-1)
         }
-        
     }
-
     const incrementCountR = ()=>{
         setCountR(round+1)
     }
-
     const decrementCountR = ()=>{
         setCountR(round-1)
     }
@@ -47,12 +44,20 @@ const App = () => {
 
     return ( 
         <>
-      
-      <div style={{ display: 'flex' , position: 'relative', top:300, backgroundColor:'white'}}>
-    <Card style={{ flex: 1, marginRight: 10, border:0}}>
-        <Card.Body style={{ width: 200 , backgroundColor:'aliceblue', position:'relative', left:300}}>
+      <div style={{ display: 'flex' , position: 'relative', top:300, backgroundColor:'white', position:'relative',left:'320px'}}>
+    <Card style={{ textAlign:'center', marginRight: 10, border:0,  backgroundColor:'aliceblue'}}>
+        <Card.Body style={{ width: 200}}>
             <Card.Title style={{textAlign:'center'}}>
-                <h1 style={{ fontSize: 30}}>Team 1</h1>
+                <h1 style={{
+                    fontSize: 30,
+                    backgroundColor:'maroon',
+                    color:'white',
+                    borderTopLeftRadius:20,
+                    position: 'relative',
+                    bottom:'1rem',
+                    width:180
+                    
+                       }}>Team 1</h1>
                 {team1}<br />
                 <Button onClick={incrementCountT1}>+</Button>
                 <Button onClick={decrementCountT1} className='btn-danger'>-</Button>
@@ -60,29 +65,42 @@ const App = () => {
         </Card.Body>
     </Card>
 
-    <Card style={{ flex: 1, marginRight: 10 ,border:0}}>
-        <Card.Body style={{ width: 200 , backgroundColor:'aliceblue', position:'relative', left:130}}>
+    <Card style={{marginRight: 10 ,border:0, backgroundColor:'aliceblue', textAlign:'center', width:100}}>
+        <Card.Body style={{ width: 200, position:'relative', right:50, top:20}}>
             <Card.Title  style={{textAlign:'center'}}>
-                <h1 style={{ fontSize: 15 }}>Round</h1>
+                <h1 style={{ fontSize: 15, position:'relative', bottom:20}}>Round</h1>
                 {round}<br />
+                <div style={{backgroundColor:'yellow'}}>
                 <Button onClick={incrementCountR}>+</Button>
                 <Button onClick={decrementCountR} className='btn-danger'>-</Button>
+                </div>
+               
             </Card.Title>
         </Card.Body>
     </Card>
 
-    <Card style={{ flex: 1 ,border:0}}>
-        <Card.Body style={{ width: 200 , backgroundColor:'aliceblue', position:'relative', left:-50}}>
+
+    <Card style={{border:0, backgroundColor:'aliceblue'}}>
+        <Card.Body style={{ width: 200 , textAlign:'center'}}>
             <Card.Title  style={{textAlign:'center'}}>
-                <h1 style={{ fontSize: 30 }}>Team 2</h1>
+                <h1 style={{ fontSize: 30,
+                    backgroundColor:'royalblue',
+                    color:'white',
+                    borderTopRightRadius:20,
+                    position: 'relative',
+                    bottom:'1rem',
+                    right:'1rem',
+                    width:180
+
+                }}>Team 2</h1>
                 {team2}<br />
+
                 <Button onClick={incrementCountT2}>+</Button>
                 <Button onClick={decrementCountT2} className='btn-danger'>-</Button>
             </Card.Title>
         </Card.Body>
     </Card>
 </div>
-
         </>
      );
 } 
