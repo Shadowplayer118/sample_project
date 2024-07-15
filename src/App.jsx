1d9e0d222dadec462a457e1c4a5a6d0b9c19cf4e
Shadowplayer118 <dhanielmalinao@gmail.com>
2024-07-15 19:49:39 +0800

    Added score board

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,88 @@
 import React, { useState } from 'react';
+import {Button} from "react-bootstrap";
+import {Card} from "react-bootstrap";
 //functional component
 
 const App = () => {
 
-    const [count, setCount] = useState(1);
+    const [team1, setCount1] = useState(1);
+    const [round, setCountR] = useState(1);
+    const [team2, setCount2] = useState(1);
 
-    const incrementCount = ()=>{
-        setCount(count+1)
+    const incrementCountT1 = ()=>{
+
+        setCount1(team1+1)
+    }
+
+    const decrementCountT1 = ()=>{
+        if(team1 == 0){
+            setCount1(0)
+        }
+        else{
+            setCount1(team1-1)
+        }
+        
+    }
+
+    const incrementCountR = ()=>{
+        setCountR(round+1)
+    }
+
+    const decrementCountR = ()=>{
+        setCountR(round-1)
+    }
+
+    const incrementCountT2 = ()=>{
+        setCount2(team2+1)
+    }
+
+    const decrementCountT2 = ()=>{
+        if(team2 == 0){
+            setCount2(0)
+        }
+        else{
+            setCount2(team2-1)
+        }
     }
 
     return ( 
         <>
-        <h1 className= "title"
-        onClick={incrementCount}
-        >
-            hello world
-        </h1>
+      
+      <div style={{ display: 'flex' , position: 'relative', top:300, backgroundColor:'white'}}>
+    <Card style={{ flex: 1, marginRight: 10, border:0}}>
+        <Card.Body style={{ width: 200 , backgroundColor:'aliceblue', position:'relative', left:300}}>
+            <Card.Title style={{textAlign:'center'}}>
+                <h1 style={{ fontSize: 30}}>Team 1</h1>
+                {team1}<br />
+                <Button onClick={incrementCountT1}>+</Button>
+                <Button onClick={decrementCountT1} className='btn-danger'>-</Button>
+            </Card.Title>
+        </Card.Body>
+    </Card>
 
-        {count} is  
-        {
-            count%2==1?" odd":" even"
-        }
-        
+    <Card style={{ flex: 1, marginRight: 10 ,border:0}}>
+        <Card.Body style={{ width: 200 , backgroundColor:'aliceblue', position:'relative', left:130}}>
+            <Card.Title  style={{textAlign:'center'}}>
+                <h1 style={{ fontSize: 15 }}>Round</h1>
+                {round}<br />
+                <Button onClick={incrementCountR}>+</Button>
+                <Button onClick={decrementCountR} className='btn-danger'>-</Button>
+            </Card.Title>
+        </Card.Body>
+    </Card>
+
+    <Card style={{ flex: 1 ,border:0}}>
+        <Card.Body style={{ width: 200 , backgroundColor:'aliceblue', position:'relative', left:-50}}>
+            <Card.Title  style={{textAlign:'center'}}>
+                <h1 style={{ fontSize: 30 }}>Team 2</h1>
+                {team2}<br />
+                <Button onClick={incrementCountT2}>+</Button>
+                <Button onClick={decrementCountT2} className='btn-danger'>-</Button>
+            </Card.Title>
+        </Card.Body>
+    </Card>
+</div>
+
         </>
      );
 } 
